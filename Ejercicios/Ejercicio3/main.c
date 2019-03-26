ca1c5f3fce11ab27be4e5568078c5a87c3bf0caf
lucassaavedra123 <lucasarielsaavedra@hotmail.com>
2019-03-25 23:29:50 -0300

    mas ejercicios y agregado del getche

--- Ejercicios/Ejercicio3/main.c
+++ Ejercicios/Ejercicio3/main.c
@@ -1,41 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <conio.h>
 #define MAXCHARACTERS 100
 
-char * darStringCharCambiado(char * string, char oldChar , char newChar){
+/*
+    Buffer de entrada
+
+    Memoria temporal.
+    Si usamos scanf, la memoria temporal puede quedar con basura
+    Se usa fflush() con stdin como primer argumento. Se limpia en el buffer de la entrada
+    del teclado
+
+*/
+
+void intercambiarCaracteres(char oldChar , char newChar, char string[MAXCHARACTERS]){
 
     int i;
 
-    for(i=0 ; i < strlen(string) ; i++){
-            if(string[i] == oldChar)
+    for(i=0 ; string[i]!='\0' ; i++){
+            if(string[i] == oldChar){
                 string[i] = newChar;
+            }
     }
 
-    return string;
+}
 
+void cargarArreglo(char arreglo[MAXCHARACTERS]){
+    int contador = 0;
+    char c = 0;
+
+    c = getche();
+
+    //El numero 10 se refiere al caracter '\r'
+    while( c!= 10 && contador < MAXCHARACTERS-1 ){
+        arreglo[contador] = c;
+        contador++;
+        c = getche();
+    }
+
+    arreglo[contador] = '\0';//asi terminan todas las cadenas
+
+    //No hace falta retonarlo
+    //C solo permite devolver una variable
+    //No un conjunto, como es un arreglo
 }
 
 int main(){
 
     char stringText[MAXCHARACTERS];
-    char oldChar;
+    char oldChar;//1 byte. Un entero ocupa 4bytes
     char newChar;
 
     printf("Ingrese texto: ");
-    scanf("%[^\n]s ", stringText);
+    cargarArreglo(stringText);
 
-    getchar();
+    printf("\n");
 
-    printf("Ingrese carácter a reemplazar: ");
-    oldChar=getchar();
+    printf("Ingrese car�cter a reemplazar: ");
+    oldChar=getche();
 
-    getchar();
+    printf("\n");
 
-    printf("Ingrese carácter de reemplazo: ");
-    newChar=getchar();
+    printf("Ingrese caracter de reemplazo: ");
+    newChar=getche();
 
-    printf("%s",darStringCharCambiado(stringText, oldChar, newChar));
+    printf("\n");
+
+    intercambiarCaracteres(oldChar,newChar,stringText);
+
+    printf("%s",stringText);
 
     return 0;
 }
--- Ejercicios/Ejercicio4/main.c
+++ Ejercicios/Ejercicio4/main.c
@@ -1,12 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #define MAX 100
 
-int cargarArr​(){return 0;}
+//FUNCIONES PROPIAS
 
-int imprimirArr(){return 0;}
+int entregarAnchoUtil(int arreglo[MAX]){
+    int i;
 
-int invertirArr(){return 0;}​
+    for(i = 0; arreglo[i] != 0; i++);//For sin cuerpo
 
-int main(){return 0;}
+    return i;
+
+}
+
+void limpiarArregloInt(int arreglo[MAX]){
+    int i;
+
+    for(i=0; i < MAX-1; i++)
+        arreglo[i]=0;
+}
+
+void cargarArr(int arreglo[MAX]){
+    int i = 0;
+    int n = 0;
+
+    scanf("%d",&n);
+
+    while( i < MAX-1 && n!=0 ){
+        arreglo[i] = n;
+        i++;
+
+        if(i < MAX - 1){
+            scanf("%d",&n);
+        }
+    }
+
+    //Nos fijamos si el usuario no termino con 0
+    if(i == MAX-1){
+        arreglo[MAX-1]=0;
+    }
+    else if (i == 0){
+        arreglo[0] = 0;
+    }
+}
+
+void imprimirArr(int arreglo[MAX]){
+    int i;
+
+    printf("\nNumeros:\n");
+
+    for(i = 0; arreglo[i] != 0; i++)
+        printf("arreglo[%d] = %d\n", i, arreglo[i]);
+}
+
+void ordenarArr(int arreglo[MAX]){
+    int x;
+    int y;
+    int nAuxiliar = 0;
+    int anchoArregloUtil = entregarAnchoUtil(arreglo);//ancho sin el numero 0
+
+    for( x = 0 ; x < anchoArregloUtil ; x++ ){
+        for( y = 0 ; y < anchoArregloUtil ; y++ ){
+            if(arreglo[x] > arreglo[y]){
+                nAuxiliar = arreglo[x];
+                arreglo[x] = arreglo[y];
+                arreglo[y] = nAuxiliar;
+            }
+        }
+    }
+}
+
+void invertirArr(int arreglo[MAX]){
+    int i;
+    int nAuxiliar = 0;
+    int anchoArregloUtil = entregarAnchoUtil(arreglo);//ancho sin el numero 0
+
+    for( i=0 ; i < anchoArregloUtil/2 ; i++ ){
+        nAuxiliar = arreglo[i];
+        arreglo[i] = arreglo[anchoArregloUtil-1-i];
+        arreglo[anchoArregloUtil-1-i] = nAuxiliar;
+    }
+
+}
+
+int main(){
+
+    int integerArray1[MAX];
+    int integerArray2[MAX];//Este sera ordenado
+
+    limpiarArregloInt(integerArray1);
+    limpiarArregloInt(integerArray2);
+
+    printf("Escriba numeros (Maximo %d numeros) para el primer arreglo y finalice con 0: ", MAX-1);
+    cargarArr(integerArray1);
+    invertirArr(integerArray1);
+
+    printf("Arreglo invertido: \n");
+    imprimirArr(integerArray1);
+
+    printf("Escriba numeros (Maximo %d numeros) para el segundo arreglo y finalice con 0: ", MAX-1);
+    cargarArr(integerArray2);
+
+    ordenarArr(integerArray2);
+
+    printf("Arreglo ordenado: \n");
+    imprimirArr(integerArray2);
+
+    return 0;
+}
